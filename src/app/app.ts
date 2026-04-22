ccd606948e100c807e6a8fe7c619f287f506eb04
guadalupeturri <magaliturri@gmail.com>
2026-04-22 12:36:12 -0300

    Realizamos componentes y router-outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
+import { Nav } from './shared/nav/nav';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
